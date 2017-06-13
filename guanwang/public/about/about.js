b28ad5825aced5a179566c94387129b05211dd0f
liuting <liuting@ecshy.com>
2017-06-13 18:04:37 +0800

    add

--- guanwang/public/about/about.js
+++ guanwang/public/about/about.js
@@ -43,4 +43,31 @@ $(function () {
 	})
 
 
+
+
+
+ 		//中心点横坐标
+        var dotLeft = ($(".container").width()-$(".dot").width())/2;
+        //中心点纵坐标
+        var dotTop = ($(".container").height()-$(".dot").height())/2;
+        //起始角度
+        var stard = 0;
+        //半径
+        var radius = 200;
+        //每一个BOX对应的角度;
+        var avd = 270/$(".box").length;
+        //每一个BOX对应的弧度;
+        var ahd = avd*Math.PI/180;
+        
+        
+        
+        //设置圆的中心点的位置
+        $(".dot").css({"left":dotLeft,"top":dotTop});
+        $(".box").each(function(index, element){
+        	console.log(index)
+            $(this).css({"left":Math.sin((ahd*index))*radius+dotLeft,"top":Math.cos((ahd*index))*radius+dotTop});
+        });
+
+
+
 })
